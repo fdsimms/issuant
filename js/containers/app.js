dc1b0a6544cf627d5bd2856585dbc6a587168ca2
Frankie <fdsimms@gmail.com>
2016-05-05 15:53:28 -0400

    Clean up style.

--- js/containers/app.js
+++ js/containers/app.js
@@ -1,6 +1,6 @@
-import React, { Component, PropTypes } from 'react'
-import { connect } from 'react-redux'
-import Header from '../components/header'
+import React, { Component, PropTypes } from "react";
+import { connect } from "react-redux";
+import Header from "../components/header";
 
 
 class App extends Component {
