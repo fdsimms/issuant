89addf612dc1818b5d6dc498aa9cb769112d86f9
Frankie <fdsimms@gmail.com>
2016-05-05 16:13:22 -0400

    Create react-router routes.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 import "babel-polyfill";
 import React from "react";
 import { render } from "react-dom";
+import { Route, Router, IndexRoute, browserHistory } from "react-router";
 import Root from "./containers/root";
 
+const routes = (
+  <Route path="/" component={Root}>
+  </Route>
+);
+
 render(
-  <Root></Root>,
+  <Router history={ browserHistory }>{routes}</Router>,
   document.getElementById("root")
 );
