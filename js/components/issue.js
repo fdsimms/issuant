3aeb682ffc0747098390a54282d97ea853bdc5c0
Frankie <fdsimms@gmail.com>
2016-05-17 13:34:04 -0400

    Continue styling issues.

--- js/components/issue.js
+++ js/components/issue.js
@@ -2,18 +2,20 @@ import React, { PropTypes } from "react";
 
 const Issue = ({ issue, shortenedBody }) => (
   <ul className="issue">
-    <div className="issue-author-stuff">
-      <li className="issue-avatar">
-        <img src={issue.user.avatar_url} />
-      </li>
-      <li className="issue-author">
-        <a href={issue.user.html_url} target="_blank">
-          {issue.user.login}
-        </a>
-      </li>
+    <li className="issue-top-bar">
+      <div className="issue-author-stuff">
+        <div className="issue-avatar">
+          <img src={issue.user.avatar_url} />
+        </div>
+      </div>
+      <div className="issue-title">#{issue.number}: {issue.title}</div>
+    </li>
+    <div className="issue-author">
+    <a href={issue.user.html_url} target="_blank">
+    {issue.user.login}
+    </a>
     </div>
     <div className="issue-content">
-      <li className="issue-title">#{issue.number}: {issue.title}</li>
       <li className="issue-body">{shortenedBody}</li>
     </div>
   </ul>
