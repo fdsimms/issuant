8294fb46ef8c177f8ca29b94083c5b783f558748
Frankie <fdsimms@gmail.com>
2016-05-17 11:51:00 -0400

    Group author info and issue content into divs.

--- js/components/issue.js
+++ js/components/issue.js
@@ -2,12 +2,16 @@ import React, { PropTypes } from "react";
 
 const Issue = ({ issue, shortenedBody }) => (
   <ul className="issue">
-    <li className="issue-title">#{issue.number}: {issue.title}</li>
-    <li className="issue-avatar">
-      <img src={issue.user.avatar_url} />
-    </li>
-    <li className="issue-author">{issue.user.login}</li>
-    <li className="issue-body">{shortenedBody}</li>
+    <div className="issue-author-stuff">
+      <li className="issue-avatar">
+        <img src={issue.user.avatar_url} />
+      </li>
+      <li className="issue-author">{issue.user.login}</li>
+    </div>
+    <div className="issue-content">
+      <li className="issue-title">#{issue.number}: {issue.title}</li>
+      <li className="issue-body">{shortenedBody}</li>
+    </div>
   </ul>
 );
 
